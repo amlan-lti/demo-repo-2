2ad68a2af50bef7454071868b46d7c863a371cfa
Amlan Sahoo <amlan.sahoo@lntinfotech.com>
2020-12-18 16:06:09 +0530

    initial commit

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,16 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int n1, int n2) {
+		System.out.println("Sum="+(n1+n2));
+	}
+	
+	public void diff(int n1, int n2) {
+		System.out.println("Difference="+(n1-n2));
+	}
+	
+	public void prod(int n1, int n2) {
+		System.out.println("conflict test");
+	}
+	//change made in github editor
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("Hello World");
+	}
+	
+	public void show2() {
+		System.out.println("Hello World 2");
+	}
+}
